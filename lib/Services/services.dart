8b6c7100f22e8188a008e104afe9708c890a5595
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-06-07 10:53:16 +0300

    The main news screen has been added

---
+++ lib/Services/services.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:news_app/model/new_model.dart';
+
+class NewsApi {
+  // Новости для главного экрана
+  List<NewsModel> dataStore = [];
+  Future<void> getNews() async {
+    Uri url = Uri.parse(
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=2092820278f44ba899471595587d3aa1");
+    var response = await http.get(url);
+    var jsonData = jsonDecode(response.body);
+    if (jsonData["status"] == 'ok') {
+      jsonData["articles"].forEach((element) {
+        if (element['urlToImage'] != null &&
+            element['description'] != null &&
+            element['author'] != null &&
+            element['content'] != null
+        ) {
+          NewsModel newsModel = NewsModel(
+            title: element['title'],
+            urlToImage: element['urlToImage'],
+            description: element['description'],
+            author: element['author'],
+            content: element['content'],
+          );
+          dataStore.add(newsModel);
+        }
+      });
+    }
+  }
+}
